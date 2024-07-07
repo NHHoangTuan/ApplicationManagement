cd643194c1027cfcdefcbf230f9b99e9dcee6dca
Nguyễn Trọng Trí <nguyentrongtri.hcmus@gmail.com>
2024-07-07 20:55:17 +0700

    Them chuc nang search, sort, phan trang cho ung vien xem ds ung tuyen

--- ApplicationManagement/ApplicationManagement/obj/Debug/net7.0-windows/EnterpriseWindow.g.i.cs
+++ ApplicationManagement/ApplicationManagement/obj/Debug/net7.0-windows/EnterpriseWindow.g.i.cs
@@ -97,13 +97,13 @@ namespace ApplicationManagement {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ApplicationManagement;component/enterprisewindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ApplicationManagement;V1.0.0.0;component/enterprisewindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\EnterpriseWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -113,7 +113,7 @@ namespace ApplicationManagement {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
